51d3bdf57e12a916f6edc8fef329c562368add61
gujunmin <3013366498@qq.com>
2020-07-30 15:56:24 +0800

    feat(cli-service): 200730

--- packages/@highly-configurable/cli-service/config/base.js
+++ packages/@highly-configurable/cli-service/config/base.js
@@ -2,11 +2,17 @@
  * @description entry / output / html / static related
  */
 
+const path = require('path');
+
 module.exports = (webpackConfig, service) => {
   webpackConfig
     .context(service.context)
     .entry('app')
       .add('./src/main.js')
+      .end()
+    .output
+      .filename('[name].[hash].js')
+      .path(path.resolve(service.context, './dist'))
       .end()
 };
 
