9f484153b0e8bd8fd0db60ddda7d4fbf7e0f231b
gujunmin <3013366498@qq.com>
2020-09-10 15:53:13 +0800

    feat(cli-service): 200910

--- packages/@highly-configurable/cli-service/__tests__/Service.test.js
+++ packages/@highly-configurable/cli-service/__tests__/Service.test.js
@@ -1,5 +1,6 @@
 jest.mock('fs');
 
+const fs = require('fs');
 const Service = require('../Service');
 
 const createMockService = (init = true, command = 'serve', args = {}) => {
@@ -17,7 +18,9 @@ beforeEach(() => {
 });
 
 afterEach(() => {
-
+  if (fs.existsSync('/highly-configurable.config.js')) {
+    fs.unlinkSync('/highly-configurable.config.js');
+  }
 });
 
 test('env setting through args', () => {
@@ -28,4 +31,20 @@ test('env setting through args', () => {
 test('env setting through command', () => {
   const service = createMockService(true, 'serve', {});
   expect(process.env.NODE_ENV).toBe('development');
+});
+
+test('load user custom options from highly-configurable.config.js', () => {
+  fs.writeFileSync('/highly-configurable.config.js', ''); // only to ensure that fs.existsSync returns true
+  jest.mock('/highly-configurable.config.js', () => ({ publicPath: '/app' }), { virtual: true });
+
+  const service = createMockService(true, 'serve', {});
+
+  expect(service.projectOptions.publicPath).toBe('/app');
+});
+
+test('register serve/build/inspect commands', () => {
+  const service = createMockService(true, 'serve', {});
+  expect(service.commands).toHaveProperty('serve');
+  expect(service.commands).toHaveProperty('build');
+  expect(service.commands).toHaveProperty('inspect');
 });
